94b892c57b811f555a5c70ee05cea0efdd00492f
jiyeonUning <yeon14735@gmail.com>
2024-11-19 13:02:26 +0900

    문제 파악을 위한 소스코드 개인 해석 주석 추가
    문제 상황 파악 :
    1. get의 반환형과 set의 읽어들이는 값 모두 MoveSpeed 본인이기 때문에, 반환된 값을 다시 초기화 하는 과정을 무한히 반복하게 되며 오버플로우가 일어나는 것으로 파악 되었다.
    2. 플레이어 움직임을 구현한 함수에서 사용된 Vector3가 정규화 되지 않아, 대각선으로 이동 시 입력 값이 두 개가 되는 만큼 속도가 x, z 모두에게 곱해져서 대각선 이동 속도가 보통의 이동속도보다 더 빨라진 것으로 파악되었다.
    
    문제 해결 방안 :
    1. 반환하는 자료형을 내부에 따로 두어 사용한다.
    2. Vector3를 정규화 하여 어느 방향으로든 Input을 통해 받는 값을 일정하게 하여서, 대각선 이동 시에도 속도가 보통일 수 있도록 한다.

--- Q_02/Assets/Scripts/PlayerStatus.cs
+++ Q_02/Assets/Scripts/PlayerStatus.cs
@@ -1,17 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerStatus : MonoBehaviour
 {
+
+    /* 문제 02
+     
+       주어진 프로젝트 내에서 제공된 스크립트는 다음의 책임을 가진다.
+    1. PalyerStatus   : 플레이어 캐릭터가 가지는 기본 능력치를 보관한다. 객체 생성 시 초기화 된다.
+    2. PlayerMovement : 유저의 입력을 받고, 플레이어 캐릭터를 이동시킨다.
+    
+    문제 사항 :
+    1. 유니티 에디터에서 Run 실행 시, 윈도우에서는 stack Overflow가 발생하며 MacOS의 유니티에서는 에디터가 강제종료된다.
+    2. 플레이어 캐릭터가 X, Z축의 입력을 동시 입력 받아 대각선으로 이동할 때, 하나의 축 기준으로 움직일 때 보다 약 1.414배 빠르게 움직인다.
+     
+    두 문제가 발생한 원인과 해결 방법을 모두 서술하시오.   */
+
+    /* 수정 진행 이전 시점에서, 유니티 실행 시 콘솔창에 출력되는 오류 :
+     
+     StackOverflowException: The requested operation caused a stack overflow.
+     PlayerStatus.set_MoveSpeed (System.Single value) (at Assets/Scripts/PlayerStatus.cs:18)
+     PlayerStatus.set_MoveSpeed (System.Single value) (at Assets/Scripts/PlayerStatus.cs:18)
+
+     . . . ( 중략 ) . . .
+
+     PlayerStatus.set_MoveSpeed (System.<message truncated>    */
+
+
+    // get의 반환형이 자기 자신이므로, set에서 설정된 값으로 자기자신이 설정되어 있는데, 동시에 get에서도 본인을 반환형을 두고 있어 
+    // 다시 set에서 값을 재설정하기 위해 다시 호출하는 과정을 반복하게 되어, 오버플로우가 일어나는 것으로 확인된다.
+
+    // 해결 방안 : 반환할 자료형을 따로 두어서, 해당 값을 읽어들이고 반환하도록 한다.
     public float MoveSpeed
     {
         get => MoveSpeed;
-        private set => MoveSpeed = value;
+        private set => MoveSpeed = value; // 문제가 발생하는 부분
     }
 
     private void Awake()
     {
+        // 게임 플레이 실행 시, MoveSpeed의 값을 5f로 설정한다.
         Init();
     }
 
